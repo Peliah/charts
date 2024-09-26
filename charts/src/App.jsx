dc8156756253b51951c6416615ca7dcb485fd2ad
Peliah <pelepoupa@gmail.com>
2024-09-26 16:07:11 +0100

    first commit

---
+++ charts/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-red-500'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
